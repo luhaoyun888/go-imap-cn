c22582601b3b19415c151ad6881e713815c2e015
luhaoyun888 <148185204+luhaoyun888@users.noreply.github.com>
2024-10-21 21:32:09 +0800

    Delete thread.go

--- thread.go
+++
@@ -1,9 +0,0 @@
-package imap
-
-// ThreadAlgorithm 表示一个线程算法。
-type ThreadAlgorithm string
-
-const (
-	ThreadOrderedSubject ThreadAlgorithm = "ORDEREDSUBJECT" // 有序主题算法
-	ThreadReferences     ThreadAlgorithm = "REFERENCES"     // 引用算法
-)
